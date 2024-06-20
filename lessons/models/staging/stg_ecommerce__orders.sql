087c059fff2b9bf50787806490d4cb6f79eb8bf8
toddbutt <todd.buttrey@gmail.com>
2024-06-20 17:57:58 -0500

    Basic Project Complete

--- lessons/models/staging/stg_ecommerce__orders.sql
+++ lessons/models/staging/stg_ecommerce__orders.sql
@@ -8,11 +8,10 @@ SELECT
     order_id,
     user_id,
     status,
-    gender,
     created_at,
     returned_at,
     shipped_at,
     delivered_at,
-    num_of_item
+    num_of_item  AS num_items_ordered
 FROM
   source
